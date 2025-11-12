303ded0d856eaefdaa819cdeeb01e5d628337835
samuelbueno <15415580735@cefet-rj.br>
2025-11-12 20:18:57 -0300

    Revert "teste"
    This reverts commit d2d6bd12cb5ac2a74d51f84bc49993b3858966c3.

---
+++ Projeto/Forms/ExcluiUsuario.cs
@@ -0,0 +1,41 @@
+﻿namespace Projeto.Forms
+{
+    public partial class ExcluiUsuario : Form
+    {
+        public ExcluiUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void button_apagar_Click(object sender, EventArgs e)
+        {
+            string senha = TxtSenha.Text;
+
+            DialogResult apagar = MessageBox.Show(
+                "Essa ação não pode ser desfeita! Está certo disso?",
+                "Confirmação",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning
+                );
+
+            if (apagar == DialogResult.Yes && Conta.ExcluirConta(senha) == 2)
+            {
+                Sessao.UsuarioAtual!.Sair();
+                MessageBox.Show(
+                        "Conta excluida com sucesso.",
+                        "Excluida",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information
+                        );
+                Close();
+            }
+            else
+                MessageBox.Show(
+                        "Erro ao tentar excluir a conta, verifique a senha inserida.",
+                        "Erro",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                        );
+        }
+    }
+}
